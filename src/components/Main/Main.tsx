d75e05bd99435fc39864d31782717af57ab4116b
Aleksandr Kubyshkin <photokub@yandex.ru>
2023-12-17 13:10:41 +0300

    Feat: верстка заголовков и списка экрана main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -8,15 +8,17 @@ interface MainPropTypes {
 const Main: React.FC<MainPropTypes> = () => {
     return (
         <section className={styles.main}>
-            <h1>Заливам.НЕТTTTTTY</h1>
-            <h2>Вас затопили?
+            <h1 className={styles.main__logo}>Заливам.НЕТ</h1>
+            <h2 className={styles.main__title}>Вас затопили?
                 Протекла крыша?
                 Соседи отрицают свою вину и отказываются возмещать ущерб?
                 Не беда! Мы поможем Вам!</h2>
-            <h3>Наши приемущества:</h3>
-            <h4>10 000</h4>
-            <p>Предоставление качественных услуг является нашей главной задачей</p>
-
+            <h3 className={styles.main__subtitle}>Наши приемущества:</h3>
+            <ul className={styles.main__list}>
+                <li className={styles.main__listitem}>Предоставление качественных услуг является нашей главной задачей</li>
+                <li className={styles.main__listitem}>Для нас важен комфорт клиента</li>
+                <li className={styles.main__listitem}>Мы делаем, а не просто говорим</li>
+            </ul>
         </section>
     )
 }
