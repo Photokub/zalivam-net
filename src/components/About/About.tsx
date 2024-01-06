bca86a02d86116f20a934670416bea8d3515df0a
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-01-06 23:16:17 +0300

    Feat: "Верстать элемент StatElement"

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.css"
+import StatElement from "../StatElement/StatElement";
 
 interface AboutPropTypes {
 
@@ -10,7 +11,10 @@ const About: React.FC<AboutPropTypes> = ({ }) => {
         <section className="about">
             <div className="about__wrap">
                 <h2 className="about__tilte">О нас в цифрах</h2>
-
+                <StatElement
+                    title="10 000"
+                    subtitle="КЛИЕНТОВ"
+                />
             </div>
         </section>
     )
---
+++ src/components/StatElement/StatElement.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./StatElement.css"
+
+interface StatElementPropTypes {
+    title: string,
+    subtitle: string
+}
+
+const StatElement: React.FC<StatElementPropTypes> = ({
+    title,
+    subtitle
+}) => {
+    return (
+        <div className="statElement">
+            <h3 className="statElement__title">{title}</h3>
+            <p className="statElement__subtitle">{subtitle}</p>
+        </div>
+    )
+}
+
+export default StatElement
