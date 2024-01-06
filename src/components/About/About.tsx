38e2883cdacb4304db62c1d34a68fe64e8e78233
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-01-06 21:46:00 +0300

    Feat: "Cоздать элемент About"

---
+++ src/components/About/About.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css"
+
+interface AboutPropTypes {
+
+}
+
+const About: React.FC<AboutPropTypes> = ({}) => {
+    return(
+        <section className="about">
+<h1>TEST</h1>
+        </section>
+    )
+}
+
+export default About 
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from '../Main/Main';
+import  About  from '../About/About';
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <About />
     </div>
   );
 }
