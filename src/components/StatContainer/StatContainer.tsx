568bd3055c772e9b821207af46931995ddc15d62
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-01-13 13:14:51 +0300

    Feat: Верстать экран About в десктопной версии

--- src/components/StatContainer/StatContainer.tsx
+++ src/components/StatContainer/StatContainer.tsx
@@ -8,29 +8,28 @@ interface StatContainerPropTypes {
 
 const StatContainer: React.FC<StatContainerPropTypes> = ({ }) => {
 
-    // const moduleSpan = "100 <span className="statElement__span">млн</span>"
-    const moduleSpan = <h3 className="statElement__title">100 <span className="statElement__span">млн</span></h3>
+    const moduleSpan = <span>100 <span className="statElement__span">МЛН</span></span>
 
-return (
-    <div className="statContainer">
-        <StatElement
-            title="10 000"
-            subtitle="КЛИЕНТОВ"
-        />
-        <StatElement
-            title = {moduleSpan}
-            subtitle="ВЫПЛАТ"
-        />
-        <StatElement
-            title="9"
-            subtitle="ЛЕТ ПРАКТИКИ"
-        />
-        <StatElement
-            title="5 000"
-            subtitle="ВЫИГРАННЫХ ДЕЛ "
-        />
-    </div>
-)
+    return (
+        <div className="statContainer">
+            <StatElement
+                title="10 000"
+                subtitle="КЛИЕНТОВ"
+            />
+            <StatElement
+                title={moduleSpan}
+                subtitle="ВЫПЛАТ"
+            />
+            <StatElement
+                title="9"
+                subtitle="ЛЕТ ПРАКТИКИ"
+            />
+            <StatElement
+                title="5 000"
+                subtitle="ВЫИГРАННЫХ ДЕЛ "
+            />
+        </div>
+    )
 }
 
 export default StatContainer; 
