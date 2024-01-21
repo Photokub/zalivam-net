e9a684090338fa60fc21da7116e29d3998a613ed
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-01-21 19:41:11 +0300

    Feat: создание компонента StepsContiner

--- src/components/Methods/Methods.tsx
+++ src/components/Methods/Methods.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import "./Methods.css"
+import "./Methods.css";
+import "../StepsContainer/StepsContainer"
+import StepsContainer from '../StepsContainer/StepsContainer';
 
 interface MethodPropTypes {
 
 }
 
-const Methods:React.FC<MethodPropTypes> = ({}) => {
-    return(
+const Methods: React.FC<MethodPropTypes> = ({ }) => {
+    return (
         <section className='methods'>
-
+            <div className="methods__warp">
+                <h2 className="methods__title">Как мы работаем, и кто мы</h2>
+                <StepsContainer />
+            </div>
         </section>
     )
 }
---
+++ src/components/StepsContainer/StepsContainer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./StepsContainer.css"
+
+interface StepsContainerProptypes {
+
+};
+
+const StepsContainer: React.FC<StepsContainerProptypes> = ({ }) => {
+    return (
+        <div className="stepsContainer">
+
+        </div>
+    )
+}
+
+export default StepsContainer;
