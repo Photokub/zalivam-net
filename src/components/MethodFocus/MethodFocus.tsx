1511a99621359907376098a577877f8df115d2eb
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-01-23 22:11:02 +0300

    Feat: верстать элемент MethdoFocus

--- src/components/MethodFocus/MethodFocus.tsx
+++ src/components/MethodFocus/MethodFocus.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./MethodFocus.css"
 
-interface MethodFocusPropTypes{
+interface MethodFocusPropTypes {
 
 }
 
-const MethodFocus: React.FC <MethodFocusPropTypes> = ({}) => {
-    return(
+const MethodFocus: React.FC<MethodFocusPropTypes> = ({ }) => {
+    return (
         <div className="methodFocus">
-
+            <p className="methodFocus__number">3</p>
+            <p className="methodFocus__title">СОСТАВЛЕНИЕ ПРЕТЕНЗИИ И ИСКОГОГО ЗАЯВЛЕНИЯ </p>
+            <p className="methodFocus__subtitle">Lorem ipsum dolor sit amet consectetur. Tincidunt tortor cras ipsum viverra libero bibendum. Ut suspendisse aliquam integer nisl.</p>
         </div>
     )
 }
