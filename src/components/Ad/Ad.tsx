23805f95c4a80611c460052974db92c31ef8a9da
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-01-30 22:36:08 +0300

    Feat: Верстать компонент Ad

--- src/components/Ad/Ad.tsx
+++ src/components/Ad/Ad.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Ad.css"
 import AdForm from "../AdForm/AdForm";
+import womenPhoto from "../../images/ad-image.png"
 
 interface AdPropTypes {
 
@@ -9,7 +10,14 @@ interface AdPropTypes {
 const Ad: React.FC<AdPropTypes> = ({ }) => {
     return (
         <section className="ad">
-            <AdForm />
+            <div className="ad__wrap">
+                <div className="ad__image">
+                    <img src={womenPhoto} alt='Девушка-юрист' id="womenPhoto" />
+                    <div className="ad__imageEffect"/>
+                </div>
+
+                <AdForm />
+            </div>
         </section>
     )
 }
