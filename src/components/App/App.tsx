f09e23497a5190bd88b5d53ca3e127409018bd65
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-02-04 21:04:11 +0300

    Feat: верстать элемент Header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,10 +9,12 @@ import Ad from '../Ad/Ad';
 import Reviews from '../Reviews/Reviews';
 import Feedback from '../Feedback/Feedback';
 import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
       <About />
       <Solutions />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PhoneIcon from "../../images/header_phone_icon.svg"
+import "./Header.css"
+
+const Header: React.FC = ({ }) => {
+    return (
+        <header className="header">
+            <p className="header__logo">ЗАЛИВАМ <span className="header__logoHighlight">НЕТ</span></p>
+            <nav className="header__nav">
+                <Link className="header__navLink" to="#">О нас</Link>
+                <Link className="header__navLink" to="#">Решения</Link>
+                <Link className="header__navLink" to="#">Как мы работаем?</Link>
+                <Link className="header__navLink" to="#">Подать заявку</Link>
+                <Link className="header__navLink" to="#">Отзывы</Link>
+                <Link className="header__navLink" to="#">Связаться с нами</Link>
+            </nav>
+            <div className="header__contacts">
+                <p className="header__contactsAdress">Москва, Красная пл. д.1</p>
+                <img src={PhoneIcon} alt="пиктограмма телефона" className="header__contacts__icon" />
+                <h4 className="header__contactsPhoneNumber">8 800 123 45 67</h4>
+            </div>
+        </header>
+    )
+}
+
+export default Header
