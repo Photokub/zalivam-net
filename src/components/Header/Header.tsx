145730156b3bd0dbbc200fbe632ef863a952ca6e
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-02-07 18:36:49 +0300

    Feat: создать элемент Navigation и внедрить его в Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PhoneIcon from "../../images/header_phone_icon.svg"
 import "./Header.css"
+import Navigation from "../Navigation/Navigation";
 
 const Header: React.FC = ({ }) => {
     return (
         <header className="header">
             <div className="header__wrap">
                 <p className="header__logo">ЗАЛИВАМ <span className="header__logoHighlight">НЕТ</span></p>
-                <nav className="header__nav">
+                {/* <nav className="header__nav">
                     <Link className="header__navLink" to="#">О нас</Link>
                     <Link className="header__navLink" to="#">Решения</Link>
                     <Link className="header__navLink" to="#">Как мы работаем?</Link>
                     <Link className="header__navLink" to="#">Подать заявку</Link>
                     <Link className="header__navLink" to="#">Отзывы</Link>
                     <Link className="header__navLink" to="#">Связаться с нами</Link>
-                </nav>
+                </nav> */}
+                <Navigation />
                 <div className="header__contacts">
                     <p className="header__contactsAdress">Москва, Красная пл. д.1</p>
                     <img src={PhoneIcon} alt="пиктограмма телефона" className="header__contacts__icon" />
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css"
+
+interface NavigationPropTypes {
+
+}
+
+const Navigation: React.FC<NavigationPropTypes> = ({ }) => {
+    return (
+        <nav className="nav">
+            <ul className="navList">
+                <li className="navList__item">
+                    <Link className="nav__link" to="#">О нас</Link>
+                </li>
+                <li className="navList__item">
+                    <Link className="nav__link" to="#">Решения</Link>
+                </li>
+                <li className="navList__item">
+                    <Link className="nav__link" to="#">Как мы работаем?</Link>
+                </li>
+                <li className="navList__item">
+                    <Link className="nav__link" to="#">Подать заявку</Link>
+                </li>
+                <li className="navList__item">
+                    <Link className="nav__link" to="#">Отзывы</Link>
+                </li>
+                <li className="navList__item">
+                    <Link className="nav__link" to="#">Связаться с нами</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
