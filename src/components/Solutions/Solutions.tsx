1cf818d1d48c7d11a2e95b3d7567bdd8fef17e13
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-03-13 21:36:04 +0300

    Refactor: убрать текстовый блок в Решениях и изменить заголовок

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -26,11 +26,11 @@ const Solutions: React.FC<SolutionsPropTypes> = ({
     return (
         <section className="solutions" id="solutions">
             <div className="solutions__wrap">
-                <h2 className="solutions__title">Решения</h2>
-                <div className="solutions__textContainer">
+                <h2 className="solutions__title">Наша судебная практика</h2>
+                {/* <div className="solutions__textContainer">
                     <h4 className="solutions__subtitle">Title</h4>
                     <p className="solutions__paragraph">Lorem ipsum dolor sit amet consectetur. Suspendisse turpis faucibus in massa non in id. Vivamus rhoncus quam mi nisl dui. Sed in integer aliquet egestas dolor pellentesque.</p>
-                </div>
+                </div> */}
                 <Carousel
                     selectedSolution={selectedSolution}
                     SolutionsArray={SolutionsArray}
