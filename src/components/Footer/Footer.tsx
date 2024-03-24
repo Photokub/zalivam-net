1be6242dada8a2128494de59005c0c0b4c600524
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-03-24 11:25:33 +0300

    Refactor: правка Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,14 +12,14 @@ const Footer: React.FC<FooterPropTypes> = ({ }) => {
     return (
         <footer className="footer">
             <div className="footer__wrap">
-                <p className="footerTitle">ЗАЛИВАМ НЕТ</p>
+                {/* <p className="footerTitle">ЗАЛИВАМ НЕТ</p> */}
                 <div className="footer__contactContainer">
                     <div className="footer__requisites">
-                        <p className="footer__paragraph">Общество с ограниченной ответственностью «ЗАЛИВАМНЕТ»</p>
-                        <p className="footer__paragraph">ИНН: 9731080512      КПП: 773101001</p>
+                        <p className="footer__paragraph">ИП Отинов А.А.<br/>ИНН 772168817907<br/>ОГРНИП 323774600460286</p>
+                        <p className="footer__paragraph">ИП Горбунов А.Б<br/>ИНН 772153219573О<br/>ГРНИП 322774600270288</p>
                     </div>
                     <div className="footer__adress">
-                        <p className="footer__paragraph">Юридический адрес:</p>
+                        <p className="footer__paragraph">Наш адрес:</p>
                         <p className="footer__paragraph">Москва, Земляной Вал 64с2, кабинет 323</p>
                     </div>
                     <div className="footer__socialMedia">
@@ -35,7 +35,7 @@ const Footer: React.FC<FooterPropTypes> = ({ }) => {
                     </div>
                     <div className="footer__phone">
                         <p className="footer__paragraph">Звоните</p>
-                        <h4 className="footer__phoneNumber">8 800 123 45 67</h4>
+                        <h4 className="footer__phoneNumber"><Link className="footer__phoneNumberLink" to={"tel:+74997148136"}>+7 (499) 714-81-36</Link></h4>
                         <p className="footer__paragraph">Бесплатно на территории РФ</p>
                     </div>
                 </div>
