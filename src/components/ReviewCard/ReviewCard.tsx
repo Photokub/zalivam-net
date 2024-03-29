10c660b015ac3c05fd9430239d02de3525fbee5b
Aleksandr Kubyshkin <photokub@yandex.ru>
2024-03-29 16:24:09 +0300

    Refactor: редизайн карточек с отзывами

--- src/components/ReviewCard/ReviewCard.tsx
+++ src/components/ReviewCard/ReviewCard.tsx
@@ -22,8 +22,8 @@ const ReviewCard: React.FC<ReviewCard> = ({
             <div className="reviewCard__reviewContainer">
                 <img src={process.env.PUBLIC_URL + avatar} className="reviewCard__photo" alt="фото автора" />
                 <blockquote className="reviewCard__blockquote">{text}</blockquote>
+                <p className="reviewCard__sign">{author}</p>
             </div>
-            <p className="reviewCard__sign">{author}</p>
         </div>
     )
 }
